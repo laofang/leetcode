caed1671ad527ac7e74cfa648f5a0db219022e72
laofang <429856304@qq.com>
2021-12-06 22:54:55 +0800

    解决第1816题，“截断句子“  1. 注意k大于单词数量的情况即可

---
+++ src/main/java/LC1816.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode1816题，截断句子
+ * 关键字：字符串、截取
+ * 主要思路：
+ * 1. 很简单，注意k大于单词数量的情况
+ */
+public class LC1816 {
+    public String truncateSentence(String s, int k) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                count++;
+            }
+            if(count == k) {
+                count = i;
+                break;
+            }
+        }
+        if(k > count) {
+            return s;
+        }
+        return s.substring(0,count);
+    }
+}
