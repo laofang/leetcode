325011520adee735615984ffe55b4d793899c593
laofang <429856304@qq.com>
2022-03-30 22:51:42 +0800

    解决第144题，”二叉树的前序遍历“ 1. 递归就可以

---
+++ src/main/java/LC144.java
@@ -0,0 +1,23 @@
+import structure.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Leetcode144题，二叉树的前序遍历
+ * 关键字：栈、树、深度优先搜索、二叉树
+ * 主要思路：
+ * 1. 递归就可以
+ */
+public class LC144 {
+    List<Integer> ans = new LinkedList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null) {
+            return ans;
+        }
+        ans.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return ans;
+    }
+}
