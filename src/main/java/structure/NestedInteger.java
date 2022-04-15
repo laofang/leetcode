5e9f1c0272fa4eddfefd5c41a6847372dfd6b0ce
laofang <429856304@qq.com>
2022-04-15 23:48:37 +0800

    解决第385题，”迷你语法分析器“ 1. 可以递归来做 2. 列表中的元素可能是数字，也可能是列表 3. 解析过程，就是对每一个元素生成一个对象，同时列表元素可以嵌套，这就是一个递归

---
+++ src/main/java/structure/NestedInteger.java
@@ -0,0 +1,11 @@
+package structure;
+
+public class NestedInteger {
+    public NestedInteger() {}
+    public NestedInteger(Integer val) {
+
+    }
+    public NestedInteger add(NestedInteger ni) {
+        return new NestedInteger();
+    }
+}
