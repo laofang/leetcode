b881ef25a4f732fc9809e993149983494cddeb81
laofang <429856304@qq.com>
2022-05-06 22:29:24 +0800

    解决第933题，”最近的请求次数“ 1. 左右指针从0开始 2. 连续乘积，如果大于k，左指针移动，此时要除以左指针前一个数字 3. 每当小于的情况，ans += r - l + 1

---
+++ src/main/java/LC933.java
@@ -0,0 +1,29 @@
+/**
+ * Leetcode933题，最近的请求次数
+ * 关键字：设计、队列、数据流
+ * 主要思路：
+ * 1. 每次ping，都用数组来记录此时的时间和当前的长度
+ * 2. 从left位置进行循环，如果nums[left] < t - 3000,left++
+ * 3. right - left就是最终答案
+ */
+public class LC933 {
+    int[] nums;
+    int left;
+    int right;
+    public LC933() {
+        left = 0;
+        right = 0;
+        nums = new int[100000];
+    }
+
+    public int ping(int t) {
+//        map.put(t,++count);
+//        int preCount = map.get(map.ceilingKey(t-3000));
+//        return count - preCount + 1;
+        nums[right++] = t;
+        while(t-3000 > nums[left]) {
+            left++;
+        }
+        return right - left;
+    }
+}
