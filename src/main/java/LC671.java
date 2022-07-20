a785ea3197a1e2bb5b5f4e5e6973b0402da20ddf
laofang <429856304@qq.com>
2022-07-20 09:34:58 +0800

    解决Leetcode1260题，二叉树中第二小的节点 1. 中序遍历，两个变量一个存最小值，一个存次小值

--- src/main/java/LC671.java
+++ src/main/java/LC671.java
@@ -4,23 +4,23 @@ import structure.TreeNode;
  * Leetcode671题，二叉树中第二小的节点
  * 关键字：树、深度优先搜索、二叉树
  * 主要思路：
- * 1.
+ * 1. 中序遍历，两个变量一个存最小值，一个存次小值
  */
 public class LC671 {
-    int min = Integer.MAX_VALUE;
+    int min = -1;
     int ans = -1;
     public int findSecondMinimumValue(TreeNode root) {
         if(root != null) {
-            if(root.val < min) {
+            if(root.val < min || min == -1) {
                 ans = min;
                 min = root.val;
             }
-            else if(root.val < ans && root.val > min) {
+            else if(root.val > min && (root.val < ans || ans == -1)) {
                 ans = root.val;
             }
             findSecondMinimumValue(root.left);
             findSecondMinimumValue(root.right);
-            return ans == Integer.MAX_VALUE? -1: ans;
+            return ans;
         }
         return -1;
     }
