a8cdc3acce0e8efea125d795c5b5394124cdbd3e
laofang <429856304@qq.com>
2023-04-24 01:50:54 +0800

    解决Leetcode836题，矩形重叠 1. 不重叠的情况只有四种，其余都重叠

--- src/main/java/LC836.java
+++ src/main/java/LC836.java
@@ -1,8 +1,11 @@
 /**
  * Leetcode836题，矩形重叠
- * 关键字：
+ * 关键字：几何、数学
  * 主要思路：
- * 1.
+ * 1. 不重叠的情况只有四种，其余都重叠
  */
 public class LC836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return !(rec1[0] >= rec2[2] || rec2[0] >= rec1[2] || rec1[1] >= rec2[3] || rec2[1] >= rec1[3]);
+    }
 }
