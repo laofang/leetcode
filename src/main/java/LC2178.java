b8f07b9852a042b2342ee315a7280e46e94b3e6b
laofang <429856304@qq.com>
2023-07-06 23:58:48 +0800

    解决Leetcode2178题，拆分成最多数目的正偶数之和

---
+++ src/main/java/LC2178.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Leetcode2178题，拆分成最多数目的正偶数之和
+ * 关键字：贪心、数学、回溯
+ * 主要思路：
+ * 1.
+ */
+public class LC2178 {
+    public List<Long> maximumEvenSplit(long finalSum) {
+        List<Long> res = new ArrayList<>();
+        if (finalSum % 2 != 0) {
+            return res;
+        }
+        for (long i = 2; i <= finalSum; i += 2) {
+            res.add(i);
+            finalSum -= i;
+        }
+        res.set(res.size() - 1, res.get(res.size() - 1) + finalSum);
+        return res;
+    }
+}
