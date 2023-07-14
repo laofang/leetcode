3be38b2c978f1c47656605090b7c452351bf37f5
laofang <429856304@qq.com>
2023-07-15 00:07:48 +0800

    解决Leetcode979题，在二叉树中分配硬币 1. 后序遍历，求出当前节点的子节点数量和当前硬币总量 2. 如果硬币比节点数多，要把多的给父节点，如果比子节点少，要向父节点申请更多硬币

--- src/main/java/LC979.java
+++ src/main/java/LC979.java
@@ -2,9 +2,10 @@ import structure.TreeNode;
 
 /**
  * Leetcode979题，在二叉树中分配硬币
- * 关键字：
+ * 关键字：树、深度优先搜索、二叉树
  * 主要思路：
- * 1.
+ * 1. 后序遍历，求出当前节点的子节点数量和当前硬币总量
+ * 2. 如果硬币比节点数多，要把多的给父节点，如果比子节点少，要向父节点申请更多硬币
  */
 public class LC979 {
     private int ans;
@@ -14,11 +15,14 @@ public class LC979 {
         return ans;
     }
 
-    private int dfs(TreeNode node) {
+    private int[] dfs(TreeNode node) {
         if (node == null)
-            return 0;
-        int d = dfs(node.left) + dfs(node.right) + node.val - 1;
-        ans += Math.abs(d);
-        return d;
+            return new int[]{0,0};
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.left);
+        int coins = left[0] + right[0] + node.val;
+        int cnt = left[1] + right[1] + 1;
+        ans += Math.abs(cnt - coins);
+        return new int[]{coins,cnt};
     }
 }
