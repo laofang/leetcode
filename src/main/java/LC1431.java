b80cda56701f22c7091d1c75249a4578e94d5f0f
laofang <429856304@qq.com>
2023-07-24 00:01:43 +0800

    解决Leetcode1431题，有最多糖果的孩子 1. 先求出拥有最大糖果的数量，再遍历每个孩子加上最大数量，如果大于等于已知最大数量就为true

--- src/main/java/LC1431.java
+++ src/main/java/LC1431.java
@@ -3,6 +3,9 @@ import java.util.List;
 
 /**
  * Leetcode1431题，有最多糖果的孩子
+ * 关键字：数组
+ * 主要思路：
+ * 1. 先求出拥有最大糖果的数量，再遍历每个孩子加上最大数量，如果大于等于已知最大数量就为true
  */
 public class LC1431 {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
