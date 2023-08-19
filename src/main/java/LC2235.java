76e036b6a126b2028787b9b0ba4d0aaee0e6e11d
laofang <429856304@qq.com>
2023-08-19 23:17:31 +0800

    解决Leetcode2235题，两整数相加 1. 目前为止做过的最简单的题

---
+++ src/main/java/LC2235.java
@@ -0,0 +1,11 @@
+/**
+ * Leetcode2235题，两整数相加
+ * 关键字：数学
+ * 主要思路：
+ * 1. leetcode中做过的最简单的题
+ */
+public class LC2235 {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
